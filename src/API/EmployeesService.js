3ab9106ec9157c65c5a2119633143136ef8b391e
NekruzRakhimov <nekruzusa111@gmail.com>
2023-08-24 17:52:57 +0500

    added employee detail info page

---
+++ src/API/EmployeesService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export default class EmployeesService {
+    // static async getAll(limit = 10, page = 1, query="", sort="", authorizedCapitalFilter="", enterpriseAgeFilter="", location_filter="") {
+    //     const response = await
+    //         axios.get("https://egback-caf44ed58dff.herokuapp.com/enterprises", {
+    //             params: {
+    //                 _limit: limit,
+    //                 _page: page,
+    //                 _query: query,
+    //                 _sort: sort,
+    //                 _authorized_capital_filter: authorizedCapitalFilter,
+    //                 _enterprise_age_filter: enterpriseAgeFilter,
+    //                 _location_filter: location_filter
+    //             }
+    //         })
+    //     return response
+    // }
+
+    static async getByID(id) {
+
+        const response = await
+            axios.get(`https://egback-caf44ed58dff.herokuapp.com/employees/${id}`)
+        return response
+    }
+}
--- src/pages/EmployeeIDPage.jsx
+++ src/pages/EmployeeIDPage.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import {useFetching} from "../hooks/useFetching";
+import EnterprisesService from "../API/EnterprisesService";
+import Loader from "../components/UI/Loader/Loader";
+import EnterpriseTabBar from "../components/UI/EnterpriseTabBar/EnterpriseTabBar";
+import {Tab} from "../components/UI/TabBar/TabBar";
+import EmployeesService from "../API/EmployeesService";
 
 const EmployeeIdPage = () => {
+    const params = useParams()
+    const [employee, setEmployee] = useState({});
+    const [fetchEmployeeByID, isLoading, error] = useFetching(async (id) => {
+        const response = await EmployeesService.getByID(params.id)
+        setEmployee(response.data)
+        console.log(employee)
+    })
+
+    useEffect(() => {
+        fetchEmployeeByID(params.id)
+    }, []);
+
     return (
         <div>
-            в разработке
+            {/*<h1>Предприятие c ID = ${params.id}</h1>*/}
+            <h3 style={{"margin": "10px"}}>{employee.full_name}</h3>
+            {isLoading
+                ? <Loader/>
+                : <div>
+                    <div className="enterprise_id_page_main">
+                        <div className="enterprise_id_page_main_item">
+                            <h2 style={{"marginBottom": 10}}>Основная информация</h2>
+                            <div><b>ID:</b> {employee.id}</div>
+                            <div><b>ФИО:</b> {employee.full_name}</div>
+                            <div><b>ИНН:</b> {employee.inn}</div>
+                            <div><b>Должность:</b> {employee.job_title}</div>
+                            <div><b>Дата рождения:</b> {employee.birth_date}</div>
+                            <div><b>Опыт работы(в годах):</b> {employee.work_experience}</div>
+
+                            {/*<h4 style={{"marginTop": 10, "marginBottom": 5}}>Классификаторы:</h4>*/}
+                            {/*<div><b>ОКВЭД:</b> {enterPrise.okved}</div>*/}
+                            {/*<div><b>ОКПО:</b> пусто</div>*/}
+                            {/*<div><b>ОКАТО:</b> {enterPrise.address}</div>*/}
+                            {/*<div><b>ОКФС:</b> пусто</div>*/}
+                            {/*<div><b>ОКОПФ:</b> пусто</div>*/}
+                            {/*<div><b>ОКОГУ:</b> пусто</div>*/}
+
+                        </div>
+                        {/*<div className="enterprise_id_page_main_sidebar">*/}
+                        {/*    <h5>Филлиалы и дочерние предприятия</h5>*/}
+                        {/*    (пусто)*/}
+                        {/*</div>*/}
+                    </div>
+                    {/*<div>*/}
+                    {/*    <EnterpriseTabBar activeKey="1"  enterprise={enterPrise} employees={employees}>*/}
+                    {/*        {enterpriseTabs.map(item => <Tab key={item.aKey} aKey={item.aKey}*/}
+                    {/*                                         title={item.title}>{item.content}</Tab>)}*/}
+                    {/*    </EnterpriseTabBar>*/}
+                    {/*</div>*/}
+                </div>
+
+            }
         </div>
     );
 };
