7a81a74143111d2dca25a9c81a62d293b6c95de7
NekruzRakhimov <nekruzusa111@gmail.com>
2023-11-09 09:24:58 +0500

    changed main-host

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -1,3 +1,3 @@
 // export const MAIN_HOST = "http://localhost:1111"
 // export const MAIN_HOST = "https://egback-caf44ed58dff.herokuapp.com"
-export const MAIN_HOST = "http://template.eastera.tj:1111"
+export const MAIN_HOST = "http://62.122.140.74:1111"
